a629f2d2e3d1fa9e4d5b56285a720b53a2d9ca12
Alexander Czigler <alexander@czigler.eu>
2019-02-19 14:51:52 +0100

    Use env instead of operator adapter

--- api/services/consents.js
+++ api/services/consents.js
@@ -1,11 +1,9 @@
-const operator = require('../adapters/operator')
-
 const area = 'cv'
 
 const defaultRequest = {
   scope: [
     {
-      domain: operator.config.clientId,
+      domain: process.env.CLIENT_ID,
       area,
       description: 'A list of your work experiences, educations, language proficiencies and so on that you have entered in the service.',
       permissions: [ 'write' ],
@@ -20,7 +18,7 @@ const addExpiry = now => obj => durationInSeconds => Object.assign({}, obj, { ex
 module.exports = {
   createDefaultRequest: addExpiry(Date.now)(defaultRequest),
   area,
-  domain: operator.config.clientId,
+  domain: process.env.CLIENT_ID,
   addExpiry // Exposed for testing purposes
 }
 
