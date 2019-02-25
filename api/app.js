dcca5abc4ecc4b1cdd06fe42187b530c186d66c9
Alexander Czigler <alexander@czigler.eu>
2019-02-25 12:53:26 +0100

    Health check route and check for operator

--- api/app.js
+++ api/app.js
@@ -13,6 +13,26 @@ const app = express()
 app.use(express.json())
 app.use('/api', logger('dev'))
 app.use('/api', routes(operator))
+app.use('/health', (req, res, next) => {
+  const status = {
+    operator: '?'
+  }
+
+  operator.connect().then(() => {
+    status.operator = 'OK'
+  })
+  .catch(error => {
+    console.log('Could not connect to operator', error)
+    status.operator = '!OK'
+  })
+  .finally(() => {
+    res.send({
+      status
+    })
+  
+    return next()
+  })
+})
 
 // Operator routes
 app.use(operator.routes)
