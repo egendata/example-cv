2c1a9331cdc30f347f3daca95d188bbd9017f6b9
Alexander Czigler <alexander@czigler.eu>
2019-02-25 16:36:53 +0100

    Improved /health for cv - chained with operator /health

--- api/app.js
+++ api/app.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const routes = require('./routes')
+const routes = require('./routes/index')
 const { saveConsent, saveConsentRequest } = require('./services/db')
 const operator = require('./adapters/operator')
 const logger = require('morgan')
-const postgres = require('./adapters/postgres')
-const redis = require('./adapters/redis')
+const health = require('./routes/health')
 
 operator.events.on('CONSENT_APPROVED', consent => {
   saveConsent(consent)
@@ -15,44 +14,7 @@ const app = express()
 app.use(express.json())
 app.use('/api', logger('dev'))
 app.use('/api', routes(operator))
-app.use('/health', (req, res, next) => {
-  const status = {
-    operator: '?'
-  }
-
-  operator.connect().then(() => {
-    status.operator = 'OK'
-  })
-  .catch(error => {
-    res.statusCode = 503
-    status.operator = '!OK'
-  })
-  .then(() => {
-    return postgres.connect()
-  })
-  .then(() => {
-    status.postgres = 'OK'
-  })
-  .catch(error => {
-    res.statusCode = 503
-    status.postgres = '!OK'
-  })
-  .then(() => {
-    if (redis.status === 'ready') {
-      status.redis = 'OK'
-    } else {
-      res.statusCode = 503
-      status.redis = '!OK'
-    }
-  })
-  .finally(() => {
-    res.send({
-      status
-    })
-  
-    return next()
-  })
-})
+app.use('/health', health)
 
 // Operator routes
 app.use(operator.routes)
