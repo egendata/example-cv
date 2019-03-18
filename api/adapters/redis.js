f05c590a763a48bfd870de02ce290ff0b11934db
Einar Persson <me@einarpersson.com>
2019-03-18 15:39:39 +0100

    Removed unnecessary test and improve error reporting

--- api/adapters/redis.js
+++ api/adapters/redis.js
@@ -5,7 +5,9 @@ const connectionString = process.env.REDIS || 'redis://:fubar@localhost:6380/'
 const redis = new Redis(connectionString, {
   retryStrategy: (times) => {
     const maxReconnectTime = 50 * 1000
-    return Math.min(times * 50, maxReconnectTime)
+    const delay = Math.min(times * 50, maxReconnectTime)
+    console.error(`Could not connect to redis, retrying in ${delay} ms`)
+    return delay
   }
 })
 
